9b7eea26e3805b9c3c0cf28744d36fdcfe5f58c4
Wallace <walinparedes3010@gmail.com>
2024-07-12 03:59:11 -0500

    chore: Add livewire-powergrid package to composer.json and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    presets: [require("./vendor/wireui/wireui/tailwind.config.js")],
+    presets: [
+        require("./vendor/wireui/wireui/tailwind.config.js"),
+        require("./vendor/power-components/livewire-powergrid/tailwind.config.js"),
+    ],
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./vendor/laravel/jetstream/**/*.blade.php",
@@ -15,6 +18,10 @@ export default {
         "./vendor/wireui/wireui/ts/**/*.ts",
         "./vendor/wireui/wireui/src/WireUi/**/*.php",
         "./vendor/wireui/wireui/src/Components/**/*.php",
+        //
+        "./app/Livewire/**/*Table.php",
+        "./vendor/power-components/livewire-powergrid/resources/views/**/*.php",
+        "./vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php",
     ],
 
     theme: {
@@ -24,6 +31,6 @@ export default {
             },
         },
     },
-
+    darkMode: "class",
     plugins: [forms, typography],
 };
